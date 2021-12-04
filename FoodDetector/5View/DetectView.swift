31296c8169811beacb8c207f6682069d979dec98
Seyeon Kim <bbbb56146@gmail.com>
2021-12-05 01:31:22 +0900

    add baseline

--- FoodDetector/5View/DetectView.swift
+++ FoodDetector/5View/DetectView.swift
@@ -9,7 +9,39 @@ import SwiftUI
 
 struct DetectView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            //UIimage
+            Text("Image")
+            
+            Spacer()
+            
+            
+            NavigationView{
+                List{
+                    Section(header: Text("영양정보")){
+                    
+                        Text("탄수화물")
+                        Text("단백질")
+                    
+                    }
+                
+                    Section(header: Text("음식")){
+                
+                        Text("밥")
+                        Text("라면")
+                        Text("배추김치")
+                
+                    }
+                
+                }
+                .navigationBarTitle(Text("총 칼로리 : kcal"))
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("식단 공유")
+            })
+        }
+        
+        
     }
 }
 
