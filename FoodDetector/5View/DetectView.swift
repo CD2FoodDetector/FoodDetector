53a32c6916eba37d454aaa7d72c66ffc4c8b1501
Seyeon Kim <bbbb56146@gmail.com>
2021-12-04 20:42:22 +0900

    Add Detect View

---
+++ FoodDetector/5View/DetectView.swift
@@ -0,0 +1,20 @@
+//
+//  DetectView.swift
+//  FoodDetector
+//
+//  Created by 김세연 on 2021/12/04.
+//
+
+import SwiftUI
+
+struct DetectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetectView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetectView()
+    }
+}
